516555c1c49e1d17936bb01ccd98c8b1e49c6841
souichirouuka1111-eng <souichirou.uka.1111@gmail.com>
2026-08-12 16:25:48 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,170 @@
 const playersData = [
-    // 選手データ
+
+    // 阪神
+    {
+        name: "近本光司",
+        team: "阪神タイガース",
+        number: 5,
+        position: "外野手",
+        batsThrows: "左投左打",
+        birth: "1994-11-09",
+        status: "支配下",
+        stats2025: {},
+        stats2026: {}
+    },
+    {
+        name: "佐藤輝明",
+        team: "阪神タイガース",
+        number: 8,
+        position: "内野手",
+        batsThrows: "右投左打",
+        birth: "1999-03-13",
+        status: "支配下",
+        stats2025: {},
+        stats2026: {}
+    },
+
+    // 巨人
+    {
+        name: "岡本和真",
+        team: "読売ジャイアンツ",
+        number: 25,
+        position: "内野手",
+        batsThrows: "右投右打",
+        birth: "1996-06-30",
+        status: "支配下",
+        stats2025: {},
+        stats2026: {}
+    },
+
+    // DeNA
+    {
+        name: "牧秀悟",
+        team: "横浜DeNAベイスターズ",
+        number: 2,
+        position: "内野手",
+        batsThrows: "右投右打",
+        birth: "1998-04-21",
+        status: "支配下",
+        stats2025: {},
+        stats2026: {}
+    },
+
+    // 広島
+    {
+        name: "小園海斗",
+        team: "広島東洋カープ",
+        number: 51,
+        position: "内野手",
+        batsThrows: "右投左打",
+        birth: "2000-06-07",
+        status: "支配下",
+        stats2025: {},
+        stats2026: {}
+    },
+
+    // 中日
+    {
+        name: "細川成也",
+        team: "中日ドラゴンズ",
+        number: 55,
+        position: "外野手",
+        batsThrows: "右投右打",
+        birth: "1998-08-04",
+        status: "支配下",
+        stats2025: {},
+        stats2026: {}
+    },
+
+    // ヤクルト
+    {
+        name: "村上宗隆",
+        team: "東京ヤクルトスワローズ",
+        number: 55,
+        position: "内野手",
+        batsThrows: "右投左打",
+        birth: "2000-02-02",
+        status: "支配下",
+        stats2025: {},
+        stats2026: {}
+    },
+
+    // 日本ハム
+    {
+        name: "万波中正",
+        team: "北海道日本ハムファイターズ",
+        number: 66,
+        position: "外野手",
+        batsThrows: "右投右打",
+        birth: "2000-04-07",
+        status: "支配下",
+        stats2025: {},
+        stats2026: {}
+    },
+
+    // ソフトバンク
+    {
+        name: "山川穂高",
+        team: "福岡ソフトバンクホークス",
+        number: 5,
+        position: "内野手",
+        batsThrows: "右投右打",
+        birth: "1991-11-23",
+        status: "支配下",
+        stats2025: {},
+        stats2026: {}
+    },
+
+    // オリックス
+    {
+        name: "宮城大弥",
+        team: "オリックス・バファローズ",
+        number: 13,
+        position: "投手",
+        batsThrows: "左投左打",
+        birth: "2001-08-25",
+        status: "支配下",
+        stats2025: {},
+        stats2026: {}
+    },
+
+    // 楽天
+    {
+        name: "浅村栄斗",
+        team: "東北楽天ゴールデンイーグルス",
+        number: 3,
+        position: "内野手",
+        batsThrows: "右投右打",
+        birth: "1990-11-12",
+        status: "支配下",
+        stats2025: {},
+        stats2026: {}
+    },
+
+    // 西武
+    {
+        name: "源田壮亮",
+        team: "埼玉西武ライオンズ",
+        number: 6,
+        position: "内野手",
+        batsThrows: "右投左打",
+        birth: "1993-02-16",
+        status: "支配下",
+        stats2025: {},
+        stats2026: {}
+    },
+
+    // ロッテ
+    {
+        name: "佐藤都志也",
+        team: "千葉ロッテマリーンズ",
+        number: 32,
+        position: "捕手",
+        batsThrows: "右投左打",
+        birth: "1998-01-27",
+        status: "支配下",
+        stats2025: {},
+        stats2026: {}
+    }
+
 ];
